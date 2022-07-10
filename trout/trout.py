916d76da2b26c6bb50cea8d4073f56ac978baad3
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-07-10 19:25:23 +0900

    add euclid fn (editing now)

--- trout/trout.py
+++ trout/trout.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-def inverse(rawdata,mod):
-    data = np.array(rawdata, dtype=int)
+def inverse(data,mod):
+    data = np.array(data, dtype=int)
 
     #--- 行列式計算 ---#
     det = np.linalg.det(data)
@@ -19,3 +19,26 @@ def inverse(rawdata,mod):
     data_inv = np.dot(data, det_inv) % mod
 
     return data_inv
+
+def euclid(a,b):
+    data = [list(range(4))]
+    data.insert(0, a)
+    data.insert(1, b)
+
+    if 0 == data[0] or 0 == data[1]: #Check suitability
+        raise Exception('ERROR:Do not assign 0 to the 1st and 2nd argument.')
+    
+    i = 0
+    while True:
+        data[i+1][2] = (data[i][0] // data[i][1])
+        data[i+1][3] = (data[i][0] % data[i][1])
+        print(f'{data[i][0]} = {data[i][1]} × {data[i][2]} + {data[i][3]}')
+
+        if data[i][3] == 0:
+            break
+
+        data[i+1][0] = data[i][1]
+        data[i+1][1] = data[i][3]
+        i += 1
+    
+    
